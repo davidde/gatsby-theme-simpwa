7323fcac5f612ba6d63a4043e0a0fbd0fdb97a7c
DavidDeprost <dadeprost@gmail.com>
2019-06-09 16:56:47 +0200

    Refactor grid and put Global in stylesheet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Global, css } from "@emotion/core"
+import { css } from "@emotion/core"
 
 
 export default ({ theme, children }) => {
@@ -11,33 +11,13 @@ export default ({ theme, children }) => {
   return (
     <div css={css`
           display: grid;
-          grid-template-columns: min-content 1fr;
-          grid-template-rows: minmax(min-content, max-content) 1fr;
-          grid-template-areas: 'leftside header rightside'
+          grid-template-columns: auto 1fr auto;
+          grid-template-rows: calc(4rem + 2px) auto;
+          grid-template-areas: 'leftside header  rightside'
                                'leftside content rightside';
     `}>
-        <Global
-          styles={css`
-            * {
-              /* margin: 0; */
-              /* padding: 0; */
-              /* box-sizing: border-box; */
-              /* border: 3px solid red; */
-            }
-            body {
-              min-height: 100vh;
-              max-width: 100vw;
-              margin: 0;
-              background: var(--bg);
-              color: var(--fg);
-              /* Disable scrolling on body: */
-              overflow: hidden;
-              font-family: "Open Sans", sans-serif;
-            }
-          `}
-        />
 
-          {children}
+      {children}
 
     </div>
   )
