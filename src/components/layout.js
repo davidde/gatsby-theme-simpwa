51d75d86fa75c88e81b4b7a07ee2a9bb2ee953b7
DavidDeprost <dadeprost@gmail.com>
2019-06-11 17:37:46 +0200

    Refactor Layout and theme

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,23 +2,32 @@ import React from "react"
 import { css } from "@emotion/core"
 
 
-export default ({ theme, children }) => {
-  if (!theme) {
-    theme = 'joy';
+class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    let theme = this.props.theme;
+    if (!theme) {
+      theme = 'joy';
+    }
+    require('./themes/theme-' + theme + '.scss');
   }
-  require('./themes/theme-' + theme + '.scss');
 
-  return (
-    <div css={css`
-          display: grid;
-          grid-template-columns: auto 1fr auto;
-          grid-template-rows: calc(4rem + 2px) auto;
-          grid-template-areas: 'leftside header  rightside'
-                               'leftside content rightside';
-    `}>
+  render() {
+    return (
+      <div css={css`
+            display: grid;
+            grid-template-columns: auto 1fr auto;
+            grid-template-rows: calc(4rem + 2px) auto;
+            grid-template-areas: 'leftside header  rightside'
+                                'leftside content rightside';
+      `}>
 
-      {children}
+        {this.props.children}
 
-    </div>
-  )
+      </div>
+    )
+  }
 }
+
+export default Layout;
