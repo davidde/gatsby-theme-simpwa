fc0cbc41e53584064efc1443f0750ac27c4589ca
DavidDeprost <dadeprost@gmail.com>
2019-07-15 00:51:38 +0200

    Refactoring attempt

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -10,22 +10,26 @@ class Sidebar extends React.Component {
     super(props);
     this.state = {
       isActive: false,
-      isMedium: this.isMediumViewport(),
     };
   }
 
   componentDidMount() {
-    window.addEventListener("resize", this.mediumViewportListener );
+    window.addEventListener("resize", this.mediumViewportListener);
   }
   componentWillUnmount() {
       window.removeEventListener("resize", this.mediumViewportListener);
   }
 
   mediumViewportListener = () => {
-    let isMedium = this.isMediumViewport();
-    // Change state to force a re-render if and only if Medium viewport changes:
-    if (this.state.isMedium !== isMedium) {
-      this.setState({ isMedium });
+    if (this.state.isActive) {
+      this.mutex();
+    }
+  }
+
+  mutex = () => {
+    let other = this.props.otherRef;
+    if ( other.state.isActive && this.isMediumViewport() ) {
+      other.setState({ isActive: false });
     }
   }
 
@@ -39,26 +43,11 @@ class Sidebar extends React.Component {
   toggleSidebar = () => {
     this.setState({isActive: !this.state.isActive});
 
-    if (this.state.isMedium && this.bothSidesActive() ) {
-      let other = this.props.otherRef;
-      other.setState({isActive: !other.state.isActive});
-    }
-  }
-
-  bothSidesActive = () => {
-    let other = this.props.otherRef;
-    let bothActive = this.state.isActive && other.state.isActive;
-    // console.log('bothActive = ', bothActive);
-    return bothActive;
+    this.mutex();
   }
 
   render() {
-    let active = this.state.isActive ? 'active' : '';console.log('render = ');
-
-    if (this.state.isMedium && this.bothSidesActive() ) {
-      let other = this.props.otherRef;
-      other.setState({isActive: !other.state.isActive});
-    }
+    let active = this.state.isActive ? 'active' : '';
 
     return (
       <div className={`sidebar ${this.props.whichSide} ${active}`}>
