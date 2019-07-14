b6a55d279d8ebd7fc8d6ef79395667c70a934a17
DavidDeprost <dadeprost@gmail.com>
2019-07-14 22:16:45 +0200

    Make refs work conditionally for medium-sized viewports

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -10,22 +10,55 @@ class Sidebar extends React.Component {
     super(props);
     this.state = {
       isActive: false,
+      isMedium: this.isMediumViewport(),
     };
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.mediumViewportListener );
+  }
+  componentWillUnmount() {
+      window.removeEventListener("resize", this.mediumViewportListener);
+  }
+
+  mediumViewportListener = () => {
+    let isMedium = this.isMediumViewport();
+    // Change state to force a re-render if and only if Medium viewport changes:
+    if (this.state.isMedium !== isMedium) {
+      this.setState({ isMedium });
+    }
+  }
+
+  isMediumViewport = () => {
+    let mediumWidthQuery = getComputedStyle(document.documentElement).getPropertyValue('--mediumWidthQuery');
+    let isMedium = window.matchMedia(mediumWidthQuery).matches;
+    // console.log('isMedium = ', isMedium);
+    return isMedium;
   }
 
   toggleSidebar = () => {
     this.setState({isActive: !this.state.isActive});
 
-    let other = this.props.otherRef;
-    console.log('other = ', other);
-
-    if (other.state.isActive) {
+    if (this.state.isMedium && this.bothSidesActive() ) {
+      let other = this.props.otherRef;
       other.setState({isActive: !other.state.isActive});
     }
   }
 
+  bothSidesActive = () => {
+    let other = this.props.otherRef;
+    let bothActive = this.state.isActive && other.state.isActive;
+    // console.log('bothActive = ', bothActive);
+    return bothActive;
+  }
+
   render() {
-    let active = this.state.isActive ? 'active' : '';
+    let active = this.state.isActive ? 'active' : '';console.log('render = ');
+
+    if (this.state.isMedium && this.bothSidesActive() ) {
+      let other = this.props.otherRef;
+      other.setState({isActive: !other.state.isActive});
+    }
 
     return (
       <div className={`sidebar ${this.props.whichSide} ${active}`}>
