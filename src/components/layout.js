4350d9acc5248c5ccc080bc73c2ed7c369e18b44
DavidDeprost <dadeprost@gmail.com>
2019-07-15 22:06:17 +0200

    Merge Viewport functions and create state

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,35 +14,40 @@ class Layout extends React.Component {
     this.state = {
       leftActive: false,
       rightActive: false,
+      isMediumViewport: null,
     }
+    this.isMediumViewport();
   }
 
   componentDidMount() {
-    window.addEventListener("resize", this.mediumViewportListener);
+    window.addEventListener("resize", this.isMediumViewport);
   }
   componentWillUnmount() {
-      window.removeEventListener("resize", this.mediumViewportListener);
-  }
-
-  mediumViewportListener = () => {
-    // If both sides are active in medium viewport, unactivate one side:
-    if ( this.state.leftActive && this.state.rightActive && this.isMediumViewport() ) {
-      this.setState({ rightActive: false });
-    }
+      window.removeEventListener("resize", this.isMediumViewport);
   }
 
   isMediumViewport = () => {
     let mediumWidthQuery = getComputedStyle(document.documentElement).getPropertyValue('--mediumWidthQuery');
-    let isMedium = window.matchMedia(mediumWidthQuery).matches;
-    // console.log('isMedium = ', isMedium);
-    return isMedium;
+    let isMediumViewport = window.matchMedia(mediumWidthQuery).matches;
+
+    if (this.state.isMediumViewport === null) {
+      // Init in constructor:
+      // eslint-disable-next-line
+      this.state.isMediumViewport = isMediumViewport;
+    } else if (this.state.isMediumViewport !== isMediumViewport) {
+      this.setState({ isMediumViewport });
+    }
+    // If both sides are active in medium viewport, unactivate one side:
+    if (this.state.leftActive && this.state.rightActive && isMediumViewport) {
+      this.setState({ rightActive: false });
+    }
   }
 
   toggleLeftSidebar = () => {
     this.setState({leftActive: !this.state.leftActive});
 
     // If other side is active in medium viewport, unactivate other side:
-    if ( this.state.rightActive && this.isMediumViewport() ) {
+    if (this.state.rightActive && this.state.isMediumViewport) {
       this.setState({ rightActive: false });
     }
   }
@@ -51,7 +56,7 @@ class Layout extends React.Component {
     this.setState({rightActive: !this.state.rightActive});
 
     // If other side is active in medium viewport, unactivate other side:
-    if ( this.state.leftActive && this.isMediumViewport() ) {
+    if (this.state.leftActive && this.state.isMediumViewport) {
       this.setState({ leftActive: false });
     }
   }
