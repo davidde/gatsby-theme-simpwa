65602f32bfed46a2058989b60ac4f1921f8116fb
DavidDeprost <dadeprost@gmail.com>
2019-07-17 18:51:26 +0200

    Attempt at getting rid of icons-header overlap

--- src/components/main/header.js
+++ src/components/main/header.js
@@ -2,9 +2,12 @@ import React from "react"
 
 
 function Header(props) {
+  let leftActive = props.leftActive ? 'leftActive' : '';
+  let rightActive = props.rightActive ? 'rightActive' : '';
+
   return (
-    <div className={`header ${props.which}`}>
-        <h1>
+    <div className={`header ${props.which} ${leftActive} ${rightActive}`}>
+        <h1 className={`${props.which} ${leftActive} ${rightActive}`}>
             {props.title}
         </h1>
     </div>
