61cccee6f346d191ab491c5b5d5416c20b38a8e0
DavidDeprost <dadeprost@gmail.com>
2019-08-12 23:30:27 +0200

    Add siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Theme Dualside',
+    author: 'David Deprost',
+    description: 'Configurable template for building simple PWA\'s',
+  },
   plugins: [
     {
       resolve: 'gatsby-plugin-compile-es6-packages',
