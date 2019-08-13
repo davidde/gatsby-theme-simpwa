10d52435004e537c74db9d544ce9913def05e05e
DavidDeprost <dadeprost@gmail.com>
2019-08-13 17:20:58 +0200

    Add theme exports to index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+export { Layout } from './src/components/layout'
+export { MainView } from './src/components/main/main-view'
+export { Leftside } from './src/components/sidebar/leftside'
+export { Rightside } from './src/components/sidebar/rightside'
