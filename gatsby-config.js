d56e3b96ef9ccbaed7853ea27564026bde8b1a8d
DavidDeprost <dadeprost@gmail.com>
2019-08-19 21:41:16 +0200

    Remove gatsby-plugin-manifest and gatsby-plugin-offline
    These plugins are not ideal when included in a theme, since they require customization.
    This is rather complicated when they aren't included in the root gatsby-config.js.
    Ideally, the developer or gatsby-starter should add these.

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,25 +11,6 @@ module.exports = {
         modules: ['gatsby-theme-simpwa'],
       },
     },
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'gatsby-theme-simpwa',
-        short_name: 'gatsby-theme-simpwa',
-        start_url: '/',
-        background_color: '#6b37bf',
-        theme_color: '#6b37bf',
-        // Enables 'Add to Homescreen' prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: 'standalone',
-        icon: 'src/images/icon.png', // This path is relative to the root of the site.
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to 'anonymous'
-        crossOrigin: 'use-credentials',
-      },
-    },
-    'gatsby-plugin-offline',
     'gatsby-plugin-sass',
   ],
 }
