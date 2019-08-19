fbadec8bf03177f579389a779eb1518cb9f94569
DavidDeprost <dadeprost@gmail.com>
2019-08-19 20:11:17 +0200

    Fix exports for installing with npm

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export { Layout } from './src/components/layout'
-export { MainView } from './src/components/main/main-view'
-export { Leftside } from './src/components/sidebar/leftside'
-export { Rightside } from './src/components/sidebar/rightside'
+export { default as Layout } from './src/components/layout'
+export { default as MainView } from './src/components/main/main-view'
+export { default as Leftside } from './src/components/sidebar/leftside'
+export { default as Rightside } from './src/components/sidebar/rightside'
