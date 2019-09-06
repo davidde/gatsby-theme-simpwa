6bf58b247f3bd3385b08ec4f958f67cd98195f5e
DavidDeprost <dadeprost@gmail.com>
2019-09-06 19:47:29 +0200

    Move scss color variables to CSS custom properties;
    Proof-of-concept for interactive theming:
    unlike scss variables, CSS custom properties are hot-swappable.

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ import Rightside from '../components/sidebar/rightside'
 
 
 export default ({ data }) => (
-  <Layout>
+  <Layout theme='joy'>
     <Leftside header='Contents' icon={faBars} >
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius semper ligula, eu luctus nunc mollis in. Vestibulum vulputate metus metus, id rutrum magna imperdiet nec. Aenean at pharetra risus. Integer convallis ac enim id aliquet. Mauris pharetra mollis nulla, eget posuere mi accumsan eu. Curabitur urna nisl, varius rhoncus tristique et, egestas et augue. Vivamus ac libero faucibus, vehicula quam in, posuere felis. Aliquam erat volutpat. Maecenas pretium iaculis dui sed elementum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
