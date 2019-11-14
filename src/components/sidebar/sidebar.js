fb36a89e7e72482421d6c317d54d569a27556d90
DavidDeprost <dadeprost@gmail.com>
2019-11-14 19:40:05 +0100

    Get rid of theming class on sidebar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Hoverbar from './hoverbar';
 import Content from '../common/content';
 import Header from '../common/header';
-import ThemeContext from '../common/contexts/theme-context';
 import SidestripContext from '../common/contexts/sidestrip-context';
 
 
@@ -11,53 +10,46 @@ function Sidebar(props) {
   let active = props.isActive ? 'active' : '';
 
   return (
-    <ThemeContext.Consumer>
+    <SidestripContext.Consumer>
     {
-      ({ theme }) => (
-        <SidestripContext.Consumer>
-        {
-          ({ sidestrip }) => (
-            <div className={`
-                sidebar
-                ${props.whichSide}
-                ${active}
-                ${theme + 'Theme'}
-                ${sidestrip === 'off' ? 'sidestripOff' : ''}
-                ${props.hasTouchscreen && (sidestrip === 'mobileOff' ||
-                                     sidestrip === 'hidden') ? 'sidestripOff' : ''}
-            `}>
-                {/* Hoverable part of the sidebar that triggers activation: */}
-                <Hoverbar
-                    whichSide={props.whichSide}
-                    icon={props.icon}
-                    isActive={props.isActive}
-                    onClick={props.toggleSidebar}
-                    hasTouchscreen={props.hasTouchscreen}
-                    sidestrip={sidestrip}
-                />
+      ({ sidestrip }) => (
+        <div className={`
+            sidebar
+            ${props.whichSide}
+            ${active}
+            ${sidestrip === 'off' ? 'sidestripOff' : ''}
+            ${props.hasTouchscreen && (sidestrip === 'mobileOff' ||
+                                  sidestrip === 'hidden') ? 'sidestripOff' : ''}
+        `}>
+            {/* Hoverable part of the sidebar that triggers activation: */}
+            <Hoverbar
+                whichSide={props.whichSide}
+                icon={props.icon}
+                isActive={props.isActive}
+                onClick={props.toggleSidebar}
+                hasTouchscreen={props.hasTouchscreen}
+                sidestrip={sidestrip}
+            />
 
-                {/* Mock background layers to hide the portrait sidebar by clicking on it: */}
-                <div
-                  className={`mock-portrait-bg ${props.whichSide} ${active}`}
-                  onClick={props.toggleSidebar}
-                />
-                <div className={`mock-portrait-bg-sub ${props.whichSide} ${active}`} />
+            {/* Mock background layers to hide the portrait sidebar by clicking on it: */}
+            <div
+              className={`mock-portrait-bg ${props.whichSide} ${active}`}
+              onClick={props.toggleSidebar}
+            />
+            <div className={`mock-portrait-bg-sub ${props.whichSide} ${active}`} />
 
-                {/* In portrait mode the active sidebar is styled differently: */}
-                <div className={`portraitSidebar ${props.whichSide} ${active}`}>
-                  <Header which={props.whichSide} title={props.header} />
+            {/* In portrait mode the active sidebar is styled differently: */}
+            <div className={`portraitSidebar ${props.whichSide} ${active}`}>
+              <Header which={props.whichSide} title={props.header} />
 
-                  <Content which={props.whichSide}>
-                      {props.children}
-                  </Content>
-                </div>
+              <Content which={props.whichSide}>
+                  {props.children}
+              </Content>
             </div>
-          )
-        }
-        </SidestripContext.Consumer>
+        </div>
       )
     }
-    </ThemeContext.Consumer>
+    </SidestripContext.Consumer>
   );
 }
 
