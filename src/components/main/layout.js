davidde/gatsby-theme-simpwa

commit 7d12c8061707c4eae1adacdfb3f4488339eb7450

Fix obscure production bug;
Sidebars refused activating on initialization (first page load)

src/components/main/layout.js
@@ -9,9 +9,9 @@ class Layout extends React.Component {
     super(props);
 
     this.state = {
-      leftActive: this.props.leftActive === 'true' ? true : false,
-      rightActive: this.props.rightActive === 'true' ? true : false,
-      mutex: this.props.mutex === 'true' ? true : false,
+      leftActive: false,
+      rightActive: false,
+      mutex: this.props.mutex,
       theme: this.props.theme,
       sidestrip: this.props.sidestrip,
       // To prevent :hover styles on mobile,
@@ -28,15 +28,15 @@ class Layout extends React.Component {
     let isMediumViewport = window.matchMedia(vars.mediumWidthQuery).matches;
     this.setState({ isMobile, isPortrait, isMediumViewport });
 
-    if (isPortrait) {
-      this.setState({
-        leftActive: false,
-        rightActive: false,
-      });
-    } else if ( this.state.leftActive &&
-                this.state.rightActive &&
-                (isMediumViewport || this.state.mutex) ) {
-        this.setState({ rightActive: false });
+    if (!isPortrait) {
+      if (this.props.leftActive) {
+        this.setState({ leftActive: true });
+        if (this.props.rightActive && !(isMediumViewport || this.state.mutex)) {
+          this.setState({ rightActive: true });
+        }
+      } else if (this.props.rightActive) {
+        this.setState({ rightActive: true });
+      }
     }
 
     let mobileClass = isMobile ? 'isMobile' : 'notMobile';
@@ -200,9 +200,9 @@ class Layout extends React.Component {
 }
 
 Layout.defaultProps = {
-  leftActive: vars.leftActive,
-  rightActive: vars.rightActive,
-  mutex: vars.mutex,
+  leftActive: vars.leftActive === 'true',
+  rightActive: vars.rightActive === 'true',
+  mutex: vars.mutex === 'true',
   theme: vars.theme,
   sidestrip: vars.sidestrip,
 };

commit a193aa976346695fa5162b062c0c6aa329e646cb

Fix obscure CustomSelect production bug on initial load;
On **initial** page load in **production** (gatsby build && gatsby serve),
CustomSelect does not show the green v checkmark of the selected option.
This works fine in development, or in production once another option is clicked.
This seems to be caused by the exported sass variables which are undefined
on the initial page load; the solution is to assign them in componentDidMount
instead of in the constructor.

This is also what caused the bug in 7d12c8061707c4eae1adacdfb3f4488339eb7450
(7 commits back).

src/components/main/layout.js
@@ -13,7 +13,7 @@ class Layout extends React.Component {
       rightActive: false,
       mutex: this.props.mutex,
       theme: this.props.theme,
-      sidestrip: this.props.sidestrip,
+      sidestrip: '',
       // To prevent :hover styles on mobile,
       // and circumvent a desktop linux firefox bug:
       hasTouchscreen: null,
@@ -26,7 +26,8 @@ class Layout extends React.Component {
     let hasTouchscreen = window.matchMedia('(hover: none)').matches;
     let isPortrait = window.matchMedia(vars.portraitQuery).matches;
     let isMediumViewport = window.matchMedia(vars.mediumWidthQuery).matches;
-    this.setState({ hasTouchscreen, isPortrait, isMediumViewport });
+    let sidestrip = this.props.sidestrip;
+    this.setState({ hasTouchscreen, isPortrait, isMediumViewport, sidestrip });
 
     if (!isPortrait) {
       if (this.props.leftActive) {