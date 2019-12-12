b63653d5de5119aa37b363f4b9b86fef9ec9c7d1
David Deprost <dadeprost@gmail.com>
2019-12-12 01:47:13 +0100

    Refactor .icon-strip without Context

--- src/components/sidebar/icon.js
+++ src/components/sidebar/icon.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import SidestripContext from '../common/contexts/sidestrip-context';
 
 
 /// Represents the inactive, hoverable sidebar that triggers activation by clicking on it:
@@ -8,32 +7,19 @@ function Icon(props) {
   let canHover = props.hasTouchscreen ? '' : 'canHover';
 
   return (
-    <SidestripContext.Consumer>
-    {
-      ({ sidestrip }) => (
-          <div
-            className={`icon ${canHover}`}
-            onClick={props.toggleSidebar}
-          >
-              {/* Square icon div: */}
-              <div className='icon-square' >
-                  <FontAwesomeIcon icon={props.icon} />
-              </div>
-
-              { // Small strip below icon to the side of the screen:
-                (sidestrip !== 'off') ?
-                    <>
-                        <div className={`icon-strip ${sidestrip} ${props.portraitOpen} ${props.landscapeOpen}`} />
-                        {/* Make icon-square and icon-strip appear as one whole: */}
-                        <div className={`icon-strip-patch ${sidestrip} ${props.portraitOpen} ${props.landscapeOpen}`} />
-                    </>
-                    :
-                    null
-              }
+      <div
+        className={`icon ${canHover}`}
+        onClick={props.toggleSidebar}
+      >
+          <div className='icon-square' >
+              <FontAwesomeIcon icon={props.icon} />
           </div>
-      )
-    }
-    </SidestripContext.Consumer>
+
+          <div className='icon-strip'>
+              <div className={`strip top ${props.portraitOpen} ${props.landscapeOpen}`} />
+              <div className={`strip bottom ${props.portraitOpen} ${props.landscapeOpen}`} />
+          </div>
+      </div>
   );
 }
 
